d827403d858bb938de29e1387410fb60cb905861
Yasin Shuman <yshuman@loyalloops.com>
2017-05-29 13:28:31 -0700

    completed project

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,27 @@
-  module.exports = function(grunt) {
+module.exports = function(grunt) {
 
-    grunt.initConfig ({
-      responsive_images: {
-        dev: {
-          options: {
-            engine: 'im',
-            sizes: [{
-              width: 800,
-              suffix: '_large_2x',
-              quality: 30
-            }]
-          },
-          files: [{
-            expand: true,
-            src: ['*.{gif,jpg,png}'],
-            cwd: 'images_src/',
-            dest: 'images/'
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            width: 800,
+            suffix: '_large_1x',
+            quality: 50
           }]
-        }
-      },
-    });
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'images_src/',
+          dest: 'images/'
+        }]
+      }
+    },
+  });
 
-    grunt.loadNpmTasks('grunt-responsive-images');
-    grunt.loadNpmTasks('grunt-contrib-imagemin')
-  grunt.registerTask('default', ['responsive_images']);};
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.registerTask('default', ['responsive_images']);
 
+};
